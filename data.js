bf0d85cb5f94c3155933bcf59d01dcab4a8c6dd3
Rattanapoom <614259058@webmail.npru.ac.th>
2020-11-10 21:24:32 +0700

    first commit

---
+++ data.js
@@ -0,0 +1,72 @@
+const data = [
+    {
+        id:1,
+        name:"Amazon Cafe",
+        type:"Drink",
+        ImageUrl:"https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/3-CYTEUGBXT2D2TJ/hero/776901be512444519763199d5fe9a272_1593408148178779119.jpeg"
+    },
+
+    {
+        id:2,
+        name:"ไก่ไม่ยอมนอน - ซอยรามบุตรี",
+        type:"Food",
+        ImageUrl:"https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/3-CZLDNAATVA2UNJ/hero/cab978976ec14ec3bffc196e49fcd919_1594554201623013383.png"
+    },
+
+    {
+        id:3,
+        name:"Hongkong Noodle (ฮ่องกงนู้ดเดิ้ล) - บางลำภู",
+        type:"Food",
+        ImageUrl:"https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/3-CY6VLJMTEXB1TN/hero/69428a4b3ef444d8b5786a424c8c5bba_1590933632384074114.jpeg"
+    },
+
+    {
+        id:4,
+        name:"จิวโจ๊กบางลำพู สุกี้กะทะร้อน - ถนนตานี",
+        type:"Food",
+        ImageUrl:"https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/THGFIST000001zo/hero/upload-photo-Hero_Photo_cb41e8148e50410a8f33632e0c336c02_1552533003406029094.jpeg"
+    },
+
+    {
+        id:5,
+        name:"รสนิยม (Rosniyom) - ท่าเรือมหาราช",
+        type:"Food",
+        ImageUrl:"https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/THGFIST00000236/hero/f7f9c1b1092e46aeb57ec06e22078fde_1598886003143737555.jpeg"
+    },
+
+    {
+        id:6,
+        name:"ข้าวหมูทอด krit - ถนนศิริพงษ์",
+        type:"Food",
+        ImageUrl:"https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/3-CZKZN8MVEUTJEA/hero/c3ab80aeebac42f59d41d3cf14a50504_1594112097107461277.jpeg"
+    },
+
+    {
+        id:7,
+        name:"Kamu Tea (คามุ ที) - วังหลัง",
+        type:"Drink",
+        ImageUrl:"https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/3-CZC1VN5CBEXGAT/hero/2cac19cd03f84037a8046cd3d9a30b85_1585793335979199515.jpeg"
+    },
+
+    {
+        id:8,
+        name:"อ้อย ข้าวคลุกกะปิ - อรุณ​อัม​รินทร์",
+        type:"Food",
+        ImageUrl:"https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/3-CZKTN2TYCTCEFA/hero/cb5b82ec7edb425daeabe9adf1fea062_1593683118171424592.jpeg"
+    },
+
+    {
+        id:9,
+        name:"Amazon Cafe",
+        type:"Drink",
+        ImageUrl:"https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/3-CYTEUGBXT2D2TJ/hero/776901be512444519763199d5fe9a272_1593408148178779119.jpeg"
+    },
+
+    {
+        id:10,
+        name:"ไก่ไม่ยอมนอน - ซอยรามบุตรี",
+        type:"Food",
+        ImageUrl:"https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/3-CZLDNAATVA2UNJ/hero/cab978976ec14ec3bffc196e49fcd919_1594554201623013383.png"
+    }
+]
+module.exports = data;
